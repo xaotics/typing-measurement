f52fabf61557dc509655c3d2d8a703ecbdc65f57
xaotics <9334902+xaotics@users.noreply.github.com>
2019-04-04 09:33:26 +0800

    Behavior changes and some bugfixes
    Moving from key-up triggering to key-down triggering and removing event listener after termination.

--- source/typing-measurement.py
+++ source/typing-measurement.py
@@ -3,15 +3,15 @@ import time
 
 def update_state(event):
     global  start_time
-    if not start_time:
+    if event.event_type == 'down' and not start_time:
         start_time = time.time()    
-    if event.event_type == 'up' and event.scan_code == 57: # when space bar released
+    if event.event_type == 'down' and event.scan_code == 57: # when space bar released
         type_sequence = ''.join(current_state)        
         if type_sequence in candidate:
             print(type_sequence.rjust(6), '|', f'{round(time.time() - start_time, 3):6.3f}' , '秒')
         start_time = None
         current_state.clear()
-    elif event.event_type == 'up':
+    elif event.event_type == 'down':
         current_state.append(event.name)
     else:
         pass
@@ -24,7 +24,9 @@ candidate = input('輸入想測的字(以空白隔開)：').split()
 print('以下開始記錄(按esc結束)')
 print('----------------------')
 
-keyboard.hook(update_state)
+k = keyboard.hook(update_state)
 keyboard.wait('esc')
+keyboard.unhook(update_state)
+
 print('----------------------')
 input('')
